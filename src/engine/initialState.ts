1ae70c8369bf98083c025eaa7569dbd8fa8fa635
Daniel Jackson <daniel.jackson210@outlook.com>
2026-06-17 01:39:48 -0400

    Updating save version

--- src/engine/initialState.ts
+++ src/engine/initialState.ts
@@ -6,7 +6,7 @@ import { PHASE_DURATIONS } from './raidStateMachine.js'
 import { createInitialLifetimeStats } from './stats.js'
 import { createStarterShieldState } from './shields.js'
 
-export const SAVE_VERSION = 2
+export const SAVE_VERSION = 3
 
 export function createInitialState(now: number = Date.now()): GameState {
   return {
